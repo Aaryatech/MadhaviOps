772a1a3db2f6c35c69e1820d4a662dd34edbdf40
ubuntu <ubuntu@ubuntu-Lenovo-ideapad-330-15IKB>
2020-01-17 18:21:36 +0530

    ops-cons

--- src/main/java/com/monginis/ops/constant/Constant.java
+++ src/main/java/com/monginis/ops/constant/Constant.java
@@ -1,29 +1,30 @@
 package com.monginis.ops.constant;
 
 public class Constant {
+//  public static final String URL ="http://localhost:8097/";
+	public static final String URL="http://107.180.91.43:8080/madhviwebapi/";
 
-	  //  //   public static final String URL ="http://localhost:8097/";
-	     public static final String URL = "http://97.74.228.55:8080/madhviapi/";
-	public static final String ITEM_IMAGE_URL = "http://97.74.228.55:8080/uploads/DAIRYITEM/";
-	public static final String SP_CAKE_FOLDER = "http://97.74.228.55:8080/uploads/SPCAKE/";
-	public static final String SPCAKE_IMAGE_URL = "http://97.74.228.55:8080/uploads/MSPCAKE/";
-	public static final String PHOTO_CAKE_URL = "http://97.74.228.55:8080/uploads/CUSTCHOICEPHOTOCAKE/";
-	public static final String FR_IMAGE_URL = "http://97.74.228.55:8080/uploads/FR/";
-	public static final String GVN_IMAGE_URL = "http://97.74.228.55:8080/uploads/GVN/";
+	     //public static final String URL = "http://97.74.228.55:8080/madhviapi/";
+	public static final String ITEM_IMAGE_URL = "http://107.180.91.43:8080/uploads/DAIRYITEM/";
+	public static final String SP_CAKE_FOLDER = "http://107.180.91.43:8080/uploads/SPCAKE/";
+	public static final String SPCAKE_IMAGE_URL = "http://107.180.91.43:8080/uploads/MSPCAKE/";
+	public static final String PHOTO_CAKE_URL = "http://107.180.91.43:8080/uploads/CUSTCHOICEPHOTOCAKE/";
+	public static final String FR_IMAGE_URL = "http://107.180.91.43:8080/uploads/FR/";
+	public static final String GVN_IMAGE_URL = "http://107.180.91.43:8080/uploads/GVN/";
 	// public static final String ReportURL ="http://localhost:8973/ops/";
-	public static final String ReportURL = "http://97.74.228.55:8080/pos/";
-	public static final String MESSAGE_IMAGE_URL = "http://97.74.228.55:8080/uploads/MSG/";
-	public static final String LOGIS_BILL_URL = "http://97.74.228.55:8080/uploads/MSPCAKE/";
-	 public static final String REPORT_SAVE = "//home/lenovo/ats-12/Report.pdf";
-	public static final String BILL_REPORT_PATH = "/opt/apache-tomcat-8.5.37/webapps/uploads/reportbill.pdf";
-	public static final String CRN_REPORT_PATH = "/opt/apache-tomcat-8.5.37/webapps/uploads/crn.pdf";
-	public static final String SELL_REPORT_PATH = "/opt/apache-tomcat-8.5.37/webapps/uploads/sellreport.pdf";
+	public static final String ReportURL = "http://107.180.91.43:8080/pos/";
+	public static final String MESSAGE_IMAGE_URL = "http://107.180.91.43:8080/uploads/MSG/";
+	public static final String LOGIS_BILL_URL = "http://107.180.91.43:8080/uploads/MSPCAKE/";
+	// public static final String REPORT_SAVE = "//home/lenovo/ats-12/Report.pdf";
+	public static final String BILL_REPORT_PATH = "/opt/apache-tomcat-8.5.49/webapps/uploads/reportbill.pdf";
+	public static final String CRN_REPORT_PATH = "/opt/apache-tomcat-8.5.49/webapps/uploads/crn.pdf";
+	public static final String SELL_REPORT_PATH = "/opt/apache-tomcat-8.5.49/webapps/uploads/sellreport.pdf";
 
-	///public static final String REPORT_SAVE = "/opt/apache-tomcat-8.5.37/webapps/uploads/Reports.pdf";
-	public static final String FACTORYNAME = "Siddharth Foods";
+	public static final String REPORT_SAVE = "/opt/apache-tomcat-8.5.49/webapps/uploads/Reports.pdf";
+	public static final String FACTORYNAME = "Madhvi Dairy";
 	public static final String FACTORYADDRESS = "C-2, A/8, Plot No. 183/1, Phase - 1 ,GIDC Naroda, Ahmedabad - 382330";
 	public static final String FACTORYGSTIN = "-";
-	public static final String CITY = "Ahmedabad";
+	public static final String CITY = "Palanpur";
 	public static final String STATE = "GUJARAT";
 	public static final int CH_IMAGE_TYPE = 0;
 	public static Integer dayOfMonthEnd = 1;
